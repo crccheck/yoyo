b52944d6418250172fdf0f27727d2f531223fcb8
crccheck <c@crccheck.com>
2016-09-27 02:52:43 +0000

    Add tests

--- src/index.js
+++ src/index.js
@@ -3,13 +3,26 @@ function merge (snippetContent, targetContent, fromIndexStrategy, indexEndStrate
   const snippetLines = snippetContent.split('\n')
   const targetLines = targetContent.split('\n')
 
+  // XXX Here are some hacks:
+  // HACK ''.split('/n') is [''] but we want []
+  if (targetLines.length === 1 && targetLines[0] === '') {
+    targetLines.pop()
+  }
+
   const fromIndex = fromIndexStrategy(snippetLines, targetLines)
   const indexEnd = indexEndStrategy(snippetLines, targetLines, fromIndex)
+  const before = targetLines.slice(0, fromIndex).join('\n')
+  const after = targetLines.slice(indexEnd).join('\n')
+  console.log(before.replace(/\n/g, '\\n') + snippetContent.replace(/\n/g, '\\n'), after.replace(/\n/g, '\\n'))
+  return before + snippetContent + after
+
+  // old way:
   targetLines.splice(
     fromIndex,
     indexEnd - fromIndex + 1,
     snippetContent.substr(0, snippetContent.length)
   )
+  console.log(targetLines)
   return targetLines.join('\n')
 }
 
