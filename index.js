7a8033cd1e13d7d5462657dd45871a6559ce97e7
crccheck <c@crccheck.com>
2016-09-27 00:29:18 +0000

    Use finders to place snippets

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const path = require('path')
 const sander = require('sander')
 const yaml = require('js-yaml')
-// const finders = require('./src/finders')
+const finders = require('./src/finders')
 
 // FIXME magic constant
 const libraryDir = '../snippets'
@@ -21,20 +21,16 @@ function loadSnippetFromLibrary (name) {
   const snippetHome = path.join(libraryDir, name)
   const templates = sander.lsrSync(snippetHome)
   templates.forEach((templatePath) => {
-    const content = fs.readFileSync(path.join(snippetHome, templatePath), 'utf8')
-    const contentLines = content.split('\n')
-    const firstLine = contentLines[0]
-    const lastLine = contentLines[contentLines.length - 2]
-    console.log(lastLine)
+    const snippetContent = fs.readFileSync(path.join(snippetHome, templatePath), 'utf8')
+    const snippetLines = snippetContent.split('\n')
 
-    const destinationContent = readOrCreateFileSync(templatePath, 'utf8')
-    const destinationLines = destinationContent.split('\n')
-    let insertAt = destinationLines.indexOf(firstLine)
-    if (insertAt === -1) {
-      insertAt = destinationLines.length
-    }
-    destinationLines.splice(insertAt, 0, content)
-    fs.writeFileSync(templatePath, destinationLines.join('\n'), 'utf8')
+    const targetContent = readOrCreateFileSync(templatePath, 'utf8')
+    const targetLines = targetContent.split('\n')
+
+    const fromIndex = finders.firstMarkerSame(snippetLines, targetLines)
+    const indexEnd = finders.lengthSameBlankLines(snippetLines, targetLines, fromIndex)
+    targetLines.splice(fromIndex, indexEnd - fromIndex, snippetContent)
+    fs.writeFileSync(templatePath, targetLines.join('\n'), 'utf8')
   })
   return templates
 }
