417db524a6be2f85cedf75cf35d14005d6884ee7
crccheck <c@crccheck.com>
2017-02-11 04:52:03 +0000

    get available snippets

---
+++ tests/test_yoyo.py
@@ -0,0 +1,7 @@
+from yoyo.yoyo import build_library, library
+
+
+def test_build_library():
+    build_library()
+    print(library)
+    return 1
--- yoyo/yoyo.py
+++ yoyo/yoyo.py
@@ -1,7 +1,22 @@
+import os
+
 import ruamel.yaml as yaml
+
+
+library = set()
+
+
+def build_library(base=None) -> None:
+    """
+    Build the internal library of what snippets are available.
+    """
+    if base is None:
+        base = os.path.join(os.path.dirname(__file__), '..', 'snippets')
+    paths = [os.path.abspath(os.path.join(base, x)) for x in os.listdir(base)]
+    for path in paths:
+        library.add(path)
+
 
 # read .yoyo.yml
 with open('.yoyo.yml') as fh:
     data = yaml.safe_load(fh)
-
-print(data)
