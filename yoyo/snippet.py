39d1f41159f30a41e3113963d7ef16f99d4e0ec8
crccheck <c@crccheck.com>
2017-03-04 06:34:29 +0000

    store what files will be used per snippet

--- yoyo/snippet.py
+++ yoyo/snippet.py
@@ -1,10 +1,13 @@
 import os
+from glob import glob
 
 
 class Snippet:
     def __init__(self, path):
         self.path = path
         self.name = os.path.basename(path)
+        # WISHLIST defer this
+        self.files = glob(f'{path}/*', recursive=True)
 
     def __str__(self):
         return self.name
