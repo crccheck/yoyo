5db2da83bf67b2dcd8363c39528fd716fd3d10d8
crccheck <c@crccheck.com>
2017-03-05 23:11:14 +0000

    actually do json

--- yoyo/merge_strategies.py
+++ yoyo/merge_strategies.py
@@ -1,2 +1,8 @@
+import json
+
+
 def merge_json(incoming, existing):
-    return '{}'
+    in_data = json.loads(incoming)
+    existing_data = json.loads(existing)
+    out_data = {**existing_data, **in_data}
+    return json.dumps(out_data, indent=2)
