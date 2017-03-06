ad8e06e806c313164bcf2ba879d48dca0d615281
crccheck <c@crccheck.com>
2017-03-06 01:37:32 +0000

    a bit more test coverage

--- tests/test_snippets.py
+++ tests/test_snippets.py
@@ -9,7 +9,13 @@ def snippet():
     return Snippet('/foo')
 
 
-def test_find_strategy(snippet):
+def test_find_strategy_works(snippet):
     template_file = File('/foo/package.json', snippet)
 
     assert find_strategy(template_file) == merge_json
+
+
+def test_find_strategy_with_nonexistent(snippet):
+    template_file = File('/foo/xyzzy', snippet)
+
+    assert find_strategy(template_file) is None
