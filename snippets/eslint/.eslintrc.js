bfc197fa7bd8fe8b76a1ae381d58e76efb792103
crccheck <c@crccheck.com>
2018-07-20 10:56:17 -0500

    Update my eslint config to what I use now

--- snippets/eslint/.eslintrc.js
+++ snippets/eslint/.eslintrc.js
@@ -6,9 +6,9 @@ module.exports = {
   rules: {
     // Lets you manipulate lines easier and have cleaner diffs
     'comma-dangle': ['error', 'always-multiline'],
-    // Two blanks for more legibile demarcation
+    // Two blank lines allow for more legibile demarcation (PEP8)
     'no-multiple-empty-lines': ['error', {max: 2}],
-    // This is often done in dev, and Uglify will clean it for prod anyways
+    // This is often done in dev, and minification will clean it for prod anyways
     'no-unreachable': 'warn',
     // Allow underlines and Flow comment syntax
     'spaced-comment': ['error', 'always', {exceptions: ['/'], markers: [':', '::']}]
---
+++ snippets/eslint/README.md
@@ -0,0 +1,6 @@
+Adds my eslint config
+---------------------
+
+Also need to run:
+
+    npm install --save-dev eslint eslint-config-standard eslint-plugin-standard eslint-plugin-promise eslint-plugin-import eslint-plugin-node
